0f5a61803eb37598708298a31879cb2adc88d6d4
Sezgin AÇIKGÖZ <sezginacikgoz@mail.com>
2025-01-16 03:55:25 +0300

    default Preheat PLA Values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2391,8 +2391,8 @@
 // Preheat Constants - Up to 10 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 195
-#define PREHEAT_1_TEMP_BED     60
+#define PREHEAT_1_TEMP_HOTEND 220
+#define PREHEAT_1_TEMP_BED     80
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     128 // Value from 0 to 255
 
