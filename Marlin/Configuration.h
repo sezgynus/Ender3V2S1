fca36ebcd049e820a3d449430d36fd1f577e721a
Sezgin AÇIKGÖZ <sezginacikgoz@mail.com>
2025-02-09 00:17:56 +0300

    prehaeat temperature updated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2392,7 +2392,7 @@
 //
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 220
-#define PREHEAT_1_TEMP_BED     80
+#define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_TEMP_CHAMBER 35
 #define PREHEAT_1_FAN_SPEED     128 // Value from 0 to 255
 
